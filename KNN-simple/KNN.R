ed86bb7d4332874bd2fc530fe1907d9b490c6ad0
subster2 <32276357+subster2@users.noreply.github.com>
2017-12-05 19:50:32 +0200

    Update KNN.R

--- KNN-simple/KNN.R
+++ KNN-simple/KNN.R
@@ -26,3 +26,21 @@ test_table_class <- data.frame(x_sign , y_sign , class_of_sign)
 k <- 7
 
 to_classify <- KNN(test_table_class , point_to_classify, k)
+
+plot(NULL, NULL, type = "l", xlim = c(min(iris[, 3]), max(iris[, 3])), ylim = c(min(iris[, 4]), max(iris[, 4])), xlab = 'Petal.Length', ylab = 'Petal.Width')
+
+colors <- c("setosa" = "red", "versicolor" = "green3",
+            "virginica" = "blue")
+col3 <- seq(from = min(iris[, 3]), to = max(iris[, 3]), by = 0.1) 
+col4 <- seq(from = min(iris[, 4]), to = max(iris[, 4]), by = 0.05) 
+for(i in col3){
+  for(l in col4){
+    z <- c(i, l)
+    xl <- iris[, 3:5]
+    class <- KNN(xl, z, k)
+    points(z[1], z[2], pch = 21, bg = "white", col = colors[class])
+  }
+};
+
+points(iris[, 3:4], pch = 21, bg = colors[iris$Species], col
+       = colors[iris$Species], asp = 1)
